75e32564114e4faba4f51d5bff98e8963bb96ced
Ahmed Jamal <gongool@hotmail.com>
2023-06-13 22:38:06 +0300

    Switch Statement

--- app.js
+++ app.js
@@ -13,12 +13,32 @@ app.get('/', (req, res) => {
     let currentDay = today.getDay();
     let day = "";
 
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
 
-
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+        default:
+            Console.log("Error , Current Day is Equal To " + currentDay);
+            break;
     }
 
     res.render("list", { kindOfDay: day });
