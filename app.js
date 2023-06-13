b5576a2bb4253d300d96590137f486d20940eb15
Ahmed Jamal <gongool@hotmail.com>
2023-06-14 01:45:27 +0300

    Modifying

--- app.js
+++ app.js
@@ -10,40 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
 
-        default:
-            Console.log("Error , Current Day is Equal To " + currentDay);
-            break;
-    }
 
-    res.render("list", { kindOfDay: day });
 
-});
+        res.render("list", { kindOfDay: day });
+
+    });
 
 app.listen(port, () => {
     console.log(`Server Is Running On Port ${port}`);
