c48a3afa92cbc517e7a18bc3c92fac970093f6ff
Ahmed Jamal <gongool@hotmail.com>
2023-06-15 19:31:32 +0300

    Adding Public folder for CSS Styling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 let items = [];
 
 app.get('/', (req, res) => {
