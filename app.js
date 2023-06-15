fd8a56144fd194ed243e396bc706fa763a53f8c5
Ahmed Jamal <gongool@hotmail.com>
2023-06-15 16:20:00 +0300

    finishing the logic of adding items through the POST request

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const port = 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({ extended: true }));
-
+let items = [];
 
 app.get('/', (req, res) => {
 
     let today = new Date();
-
     let options = {
         weekday: "long",
         day: "numeric",
@@ -18,10 +16,18 @@ app.get('/', (req, res) => {
     };
 
     let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { kindOfDay: day });
+    res.render("list", { kindOfDay: day, newListItems: items });
 
 });
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    if (item.trim() !== "") {
+        items.push(item);
+    }
+    res.redirect("/");
+});
+
 
 app.listen(port, () => {
     console.log(`Server Is Running On Port ${port}`);
