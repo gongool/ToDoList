46d236b7d8f788f9832709211da7254cd24ef1ab
Ahmed Jamal <gongool@hotmail.com>
2023-06-15 22:52:50 +0300

    Fixing the redirecting and adding listITems for both (/work) route and (/) route.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 let items = [];
+let workItems = [];
 
 app.get('/', (req, res) => {
-
     let today = new Date();
     let options = {
         weekday: "long",
@@ -17,19 +17,28 @@ app.get('/', (req, res) => {
     };
 
     let day = today.toLocaleDateString("en-US", options);
-    res.render("list", { kindOfDay: day, newListItems: items });
-
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
     if (item.trim() !== "") {
-        items.push(item);
+        if (req.body.list === "Work") {
+            workItems.push(item);
+            res.redirect("/work");
+        } else {
+            items.push(item);
+            res.redirect("/");
+        }
+    } else {
+        res.redirect("/");
     }
-    res.redirect("/");
 });
 
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
 
 app.listen(port, () => {
-    console.log(`Server Is Running On Port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
